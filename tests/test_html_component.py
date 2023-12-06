7fc0f2fcd214b1b038a9e69cde8e78c395ecfed0
Gavin <gavin104704@gmail.com>
2023-12-06 19:08:57 +0800

    a

--- tests/test_html_component.py
+++ tests/test_html_component.py
@@ -2,18 +2,21 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 import unittest
+import os.path
 
 class HTMLComponentTest(unittest.TestCase):
     def test_style(self):
         srv=Service(r"C:\Users\wc018\Driver\chromedriver.exe")
         drv=webdriver.Chrome(service=srv)
-        drv.get(r"C:\Users\wc018\Desktop\Python\justpy\tests\html_component_test_page.html")
+        drv.get(repr(os.path.join("tests","html_component_test_page.html")))
         drv.find_element(By.XPATH ,"//div[text()='test']")
         drv.close()
     
     def test_classes(self):
         srv=Service(r"C:\Users\wc018\Driver\chromedriver.exe")
         drv=webdriver.Chrome(service=srv)
-        drv.get(r"C:\Users\wc018\Desktop\Python\justpy\tests\html_component_test_page.html")
+        drv.get(repr(os.path.join("tests","html_component_test_page.html")))
         self.assertEquals(drv.find_element(By.ID,"a123").value_of_css_property("font-size"),"36px")
         drv.close()
+
+print(os.path.join("tests","html_component_test_page.html"))
