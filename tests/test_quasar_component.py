359054aceb65ee029e93a6752f5c3bc7fe3d0e4f
Gavin <gavin104704@gmail.com>
2024-01-03 19:02:20 +0800

    test_slot

--- tests/test_quasar_component.py
+++ tests/test_quasar_component.py
@@ -1,4 +1,6 @@
 from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.chrome.service import Service
 import unittest
 import os.path
@@ -11,4 +13,11 @@ class QuasarComponentTest(unittest.TestCase):
         cwd=os.getcwd()
         drv.get(os.path.join(cwd,"tests","quasar_component_test_page.html"))
         drv.find_element(By.XPATH ,"//div[text()='test']")
+        drv.close()
+
+    def test_slot(self):
+        drv=SeleniumBrowsers(headless=False).getFirst()
+        cwd=os.getcwd()
+        drv.get(os.path.join(cwd,"tests","quasar_component_test_page.html"))
+        WebDriverWait(drv,10).until(EC.presence_of_element_located((By.TAG_NAME ,"circle")))
         drv.close()
