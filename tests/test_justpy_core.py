c01545e5fca701f67c1dc00ac7878457f0149303
Gavin <gavin104704@gmail.com>
2024-03-27 19:39:27 +0800

    test

--- tests/test_justpy_core.py
+++ tests/test_justpy_core.py
@@ -5,11 +5,23 @@ Created on 2022-08-24
 """
 import justpy as jp
 from tests.base_server_test import BaseAsynctest
+import tempfile
 
 class TestJustpyCore(BaseAsynctest):
     """
     Tests for Justpy Core features
     """
+    async def asyncSetUp(
+        self,
+        port: int = 8123,
+        host: str = "127.0.0.1",
+        sleep_time=None,
+        with_server=True,
+        debug=False,
+        profile=True,
+        mode=None,
+    ):
+        await super().asyncSetUp(mode="process")
 
     async def setUp(self):
         await BaseAsynctest.setUp(self, port=8122, debug=True)
@@ -33,5 +45,6 @@ class TestJustpyCore(BaseAsynctest):
         debug = True
         if debug:
             print(html)
-            with open("/tmp/justpy.html", "w") as html_file:
-                print(html, file=html_file)
+            tmp=tempfile.NamedTemporaryFile(mode="w+t",delete=True)
+            tmp.write(html)
+            tmp.close()
